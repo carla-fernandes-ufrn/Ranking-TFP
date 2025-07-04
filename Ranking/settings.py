bf25722b262c243f3e24d08b9730e924a63ddac3
Carla Fernandes <166626231+carla-fernandes-ufrn@users.noreply.github.com>
2025-07-04 14:48:38 -0300

    Update settings.py

--- Ranking/settings.py
+++ Ranking/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATICFILES_DIRS = [STATIC_DIR]
 STATICFILES_DIRS = [
-    BASE_DIR / 'assets',
+    BASE_DIR / 'static',
 ]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_ROOT = BASE_DIR / 'staticfiles'
